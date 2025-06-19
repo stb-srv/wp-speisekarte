b85093717933f19cd21f98b2a0de66eed1290cc0
stb-srv <s.behncke@icloud.com>
2025-06-19 07:45:15 +0200

    Equalize accordion header height

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,4 +1,18 @@
 jQuery(function($){
+    function equalizeToggles(){
+        var max = 0;
+        $('.speisekarte-toggle').css('min-height','').each(function(){
+            var h = $(this).outerHeight();
+            if(h > max) max = h;
+        });
+        if(max){
+            $('.speisekarte-toggle').css('min-height', max);
+        }
+    }
+
+    equalizeToggles();
+    $(window).on('resize', equalizeToggles);
+
     $('.speisekarte-toggle').on('click', function(){
         $(this).toggleClass('active').next('.speisekarte-content').slideToggle();
     });
