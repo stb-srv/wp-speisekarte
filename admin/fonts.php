01b65aeed948c692b28386d4e8b7da391967f0e2
stb-srv <s.behncke@icloud.com>
2025-06-20 09:57:58 +0200

    Fix color fields to load saved settings

--- admin/fonts.php
+++ admin/fonts.php
@@ -1,32 +1,34 @@
 <?php
 if (!defined('ABSPATH')) exit;
+$options            = get_option('wp_speisekarte_settings', []);
 $font_family       = get_option('speisekarte_font_family', '');
 $item_font_family  = get_option('speisekarte_item_font_family', '');
 $item_font_size    = get_option('speisekarte_item_font_size', '');
 $item_font_weight  = get_option('speisekarte_item_font_weight', '');
 $item_font_style   = get_option('speisekarte_item_font_style', '');
 
-$zusatz_farbe         = get_option('zusatz_farbe', '#992766');
-$kategorie_farbe      = get_option('kategorie_farbe', '#000000');
-$speisen_farbe        = get_option('speisen_farbe', '#000000');
-$preis_farbe          = get_option('preis_farbe', '#000000');
-$zusatz_farbe_dark    = get_option('zusatz_farbe_dark', '#DD9933');
-$kategorie_farbe_dark = get_option('kategorie_farbe_dark', '#DD9933');
-$speisen_farbe_dark   = get_option('speisen_farbe_dark', '#FFFFFF');
-$preis_farbe_dark     = get_option('preis_farbe_dark', '#FFFFFF');
+$zusatz_farbe         = $options['zusatz_farbe'] ?? '#992766';
+$kategorie_farbe      = $options['kategorie_farbe'] ?? '#000000';
+$speisen_farbe        = $options['speisen_farbe'] ?? '#000000';
+$preis_farbe          = $options['preis_farbe'] ?? '#000000';
+$zusatz_farbe_dark    = $options['zusatz_farbe_dark'] ?? '#DD9933';
+$kategorie_farbe_dark = $options['kategorie_farbe_dark'] ?? '#DD9933';
+$speisen_farbe_dark   = $options['speisen_farbe_dark'] ?? '#FFFFFF';
+$preis_farbe_dark     = $options['preis_farbe_dark'] ?? '#FFFFFF';
 if (isset($_POST['fonts_save'])) {
     check_admin_referer('speisekarte_fonts_save');
+    $input = $_POST['wp_speisekarte_settings'] ?? [];
     $font_family = sanitize_text_field($_POST['font_family']);
-$zusatz_farbe      = sanitize_hex_color($_POST['zusatz_farbe']) ?: '#992766';
-$kategorie_farbe   = sanitize_hex_color($_POST['kategorie_farbe']) ?: '#000000';
-$speisen_farbe     = sanitize_hex_color($_POST['speisen_farbe']) ?: '#000000';
-$preis_farbe       = sanitize_hex_color($_POST['preis_farbe']) ?: '#000000';
-$item_font_family  = sanitize_text_field($_POST['item_font_family']);
-$item_font_size    = sanitize_text_field($_POST['item_font_size']);
-$zusatz_farbe_dark    = sanitize_hex_color($_POST['zusatz_farbe_dark']) ?: '#DD9933';
-$kategorie_farbe_dark = sanitize_hex_color($_POST['kategorie_farbe_dark']) ?: '#DD9933';
-$speisen_farbe_dark   = sanitize_hex_color($_POST['speisen_farbe_dark']) ?: '#FFFFFF';
-$preis_farbe_dark     = sanitize_hex_color($_POST['preis_farbe_dark']) ?: '#FFFFFF';
+    $zusatz_farbe      = sanitize_hex_color($input['zusatz_farbe'] ?? '') ?: '#992766';
+    $kategorie_farbe   = sanitize_hex_color($input['kategorie_farbe'] ?? '') ?: '#000000';
+    $speisen_farbe     = sanitize_hex_color($input['speisen_farbe'] ?? '') ?: '#000000';
+    $preis_farbe       = sanitize_hex_color($input['preis_farbe'] ?? '') ?: '#000000';
+    $item_font_family  = sanitize_text_field($_POST['item_font_family']);
+    $item_font_size    = sanitize_text_field($_POST['item_font_size']);
+    $zusatz_farbe_dark    = sanitize_hex_color($input['zusatz_farbe_dark'] ?? '') ?: '#DD9933';
+    $kategorie_farbe_dark = sanitize_hex_color($input['kategorie_farbe_dark'] ?? '') ?: '#DD9933';
+    $speisen_farbe_dark   = sanitize_hex_color($input['speisen_farbe_dark'] ?? '') ?: '#FFFFFF';
+    $preis_farbe_dark     = sanitize_hex_color($input['preis_farbe_dark'] ?? '') ?: '#FFFFFF';
     $item_font_weight = isset($_POST['item_font_weight']) ? 'bold' : '';
     $item_font_style  = isset($_POST['item_font_style']) ? 'italic' : '';
 
@@ -36,14 +38,15 @@ $preis_farbe_dark     = sanitize_hex_color($_POST['preis_farbe_dark']) ?: '#FFFF
     update_option('speisekarte_item_font_weight', $item_font_weight);
     update_option('speisekarte_item_font_style', $item_font_style);
 
-    update_option('zusatz_farbe', $zusatz_farbe);
-    update_option('kategorie_farbe', $kategorie_farbe);
-    update_option('speisen_farbe', $speisen_farbe);
-    update_option('preis_farbe', $preis_farbe);
-    update_option('zusatz_farbe_dark', $zusatz_farbe_dark);
-    update_option('kategorie_farbe_dark', $kategorie_farbe_dark);
-    update_option('speisen_farbe_dark', $speisen_farbe_dark);
-    update_option('preis_farbe_dark', $preis_farbe_dark);
+    $options['zusatz_farbe']      = $zusatz_farbe;
+    $options['kategorie_farbe']   = $kategorie_farbe;
+    $options['speisen_farbe']     = $speisen_farbe;
+    $options['preis_farbe']       = $preis_farbe;
+    $options['zusatz_farbe_dark']    = $zusatz_farbe_dark;
+    $options['kategorie_farbe_dark'] = $kategorie_farbe_dark;
+    $options['speisen_farbe_dark']   = $speisen_farbe_dark;
+    $options['preis_farbe_dark']     = $preis_farbe_dark;
+    update_option('wp_speisekarte_settings', $options);
     echo '<div class="updated notice"><p>Einstellungen gespeichert.</p></div>';
 }
 ?>
@@ -101,19 +104,19 @@ $preis_farbe_dark     = sanitize_hex_color($_POST['preis_farbe_dark']) ?: '#FFFF
             <table class="form-table" role="presentation">
                 <tr>
                     <th scope="row"><label for="zusatz_farbe">Zusatzinformationen</label></th>
-                    <td><input type="text" id="zusatz_farbe" name="zusatz_farbe" value="<?php echo esc_attr($zusatz_farbe); ?>" class="color-picker" data-default-color="#992766"></td>
+                    <td><input type="text" id="zusatz_farbe" name="wp_speisekarte_settings[zusatz_farbe]" value="<?php echo esc_attr($zusatz_farbe); ?>" class="color-picker" data-default-color="#992766"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="kategorie_farbe">Kategorie</label></th>
-                    <td><input type="text" id="kategorie_farbe" name="kategorie_farbe" value="<?php echo esc_attr($kategorie_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
+                    <td><input type="text" id="kategorie_farbe" name="wp_speisekarte_settings[kategorie_farbe]" value="<?php echo esc_attr($kategorie_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="speisen_farbe">Speisen</label></th>
-                    <td><input type="text" id="speisen_farbe" name="speisen_farbe" value="<?php echo esc_attr($speisen_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
+                    <td><input type="text" id="speisen_farbe" name="wp_speisekarte_settings[speisen_farbe]" value="<?php echo esc_attr($speisen_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="preis_farbe">Preisfarbe</label></th>
-                    <td><input type="text" id="preis_farbe" name="preis_farbe" value="<?php echo esc_attr($preis_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
+                    <td><input type="text" id="preis_farbe" name="wp_speisekarte_settings[preis_farbe]" value="<?php echo esc_attr($preis_farbe); ?>" class="color-picker" data-default-color="#000000"></td>
                 </tr>
             </table>
         </fieldset>
@@ -123,19 +126,19 @@ $preis_farbe_dark     = sanitize_hex_color($_POST['preis_farbe_dark']) ?: '#FFFF
             <table class="form-table" role="presentation">
                 <tr>
                     <th scope="row"><label for="zusatz_farbe_dark">Zusatzinformationen (Dark Mode)</label></th>
-                    <td><input type="text" id="zusatz_farbe_dark" name="zusatz_farbe_dark" value="<?php echo esc_attr($zusatz_farbe_dark); ?>" class="color-picker" data-default-color="#DD9933"></td>
+                    <td><input type="text" id="zusatz_farbe_dark" name="wp_speisekarte_settings[zusatz_farbe_dark]" value="<?php echo esc_attr($zusatz_farbe_dark); ?>" class="color-picker" data-default-color="#DD9933"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="kategorie_farbe_dark">Kategorie (Dark Mode)</label></th>
-                    <td><input type="text" id="kategorie_farbe_dark" name="kategorie_farbe_dark" value="<?php echo esc_attr($kategorie_farbe_dark); ?>" class="color-picker" data-default-color="#DD9933"></td>
+                    <td><input type="text" id="kategorie_farbe_dark" name="wp_speisekarte_settings[kategorie_farbe_dark]" value="<?php echo esc_attr($kategorie_farbe_dark); ?>" class="color-picker" data-default-color="#DD9933"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="speisen_farbe_dark">Speisen (Dark Mode)</label></th>
-                    <td><input type="text" id="speisen_farbe_dark" name="speisen_farbe_dark" value="<?php echo esc_attr($speisen_farbe_dark); ?>" class="color-picker" data-default-color="#FFFFFF"></td>
+                    <td><input type="text" id="speisen_farbe_dark" name="wp_speisekarte_settings[speisen_farbe_dark]" value="<?php echo esc_attr($speisen_farbe_dark); ?>" class="color-picker" data-default-color="#FFFFFF"></td>
                 </tr>
                 <tr>
                     <th scope="row"><label for="preis_farbe_dark">Preisfarbe (Dark Mode)</label></th>
-                    <td><input type="text" id="preis_farbe_dark" name="preis_farbe_dark" value="<?php echo esc_attr($preis_farbe_dark); ?>" class="color-picker" data-default-color="#FFFFFF"></td>
+                    <td><input type="text" id="preis_farbe_dark" name="wp_speisekarte_settings[preis_farbe_dark]" value="<?php echo esc_attr($preis_farbe_dark); ?>" class="color-picker" data-default-color="#FFFFFF"></td>
                 </tr>
             </table>
         </fieldset>
